6eda84e60d4baeeef41ed0c69a12902df2eaace0
Lufei Deng <lufei.deng@syndigo.com>
2023-03-03 23:12:43 -0500

    fix issue with logging

--- assignment2/helper.py
+++ assignment2/helper.py
@@ -4,4 +4,4 @@ import time
 def log(message):
     current_time = time.time()
     current_time_str = datetime.datetime.fromtimestamp(current_time).strftime('%Y-%m-%d %H:%M:%S')
-    return f'{current_time_str}: {message}'
+    print(f'{current_time_str}: {message}')
