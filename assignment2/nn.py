9ee4df9b6c89fc68d71d36605b9b895d0e9eed62
Lufei Deng <lufei.deng@syndigo.com>
2023-03-05 23:35:41 -0500

    assignmet2

--- assignment2/nn.py
+++ assignment2/nn.py
@@ -1,207 +1,72 @@
-import sys
-import time
-
+from sklearn.datasets import load_breast_cancer
 import matplotlib.pyplot as plt
-import mlrose_hiive as mlr
+import mlrose_hiive
 import numpy as np
-import pandas as pd
-from helper import log
-from imblearn.over_sampling import RandomOverSampler
-from mlrose_hiive import ExpDecay
+import time
+from sklearn.metrics import accuracy_score
+from sklearn.model_selection import learning_curve, train_test_split
 from sklearn import preprocessing
-from sklearn.metrics import (accuracy_score, f1_score, precision_score,
-                             recall_score, roc_auc_score)
-from sklearn.model_selection import (ShuffleSplit, cross_validate,
-                                     train_test_split)
 
 
-def load_data():
-    np.random.seed(2636456)
+def plot_learning_curve(solver, X, y, title, fname):
+    full_name = fname
+    train_szs, train_scores, test_scores = learning_curve(solver, X, y, train_sizes=np.linspace(0.1, 1.0, 7))
 
-    breastCancer = pd.read_csv('assignment2/breastCancer.csv', sep=',')
-    X = breastCancer.values[:, 0:-1]
-    y = breastCancer.values[:, -1]
+    tr_means = np.mean(train_scores, axis=1)
+    tr_stdev = np.std(train_scores, axis=1)
+    tt_means = np.mean(test_scores, axis=1)
+    tt_stdev = np.std(test_scores, axis=1)
+    norm_train_szs = (train_szs / max(train_szs)) * 100.0
 
-    ros = RandomOverSampler(random_state=0)
-    X_res, y_res=ros.fit_resample(X, y)
-
-    breastCancer_training_X, breastCancer_test_X, breastCancer_training_y, breastCancer_test_y = train_test_split(
-        X_res, y_res, random_state=42, test_size=0.25)
-
-    scaler = preprocessing.StandardScaler()
-    scaler_breastCancer_training_x = scaler.fit_transform(breastCancer_training_X)
-    scaler_breastCancer_test_x = scaler.fit_transform(breastCancer_test_X)
-
-    return scaler_breastCancer_training_x, breastCancer_training_y, scaler_breastCancer_test_x, breastCancer_test_y
-
-def learning_curve(estimator, trainX, trainy):
-
-    train_sizes = (np.linspace(.25, 1.0, 5) * len(trainy)).astype('int')
-
-    train_results =[]
-    test_results = []
-
-    for size in train_sizes:
-  
-        result = cross_validate(estimator, trainX, trainy, cv = ShuffleSplit(n_splits=50, test_size=0.25, random_state=0), n_jobs=4, return_train_score=True)
-
-        train_results.append(result['train_score'])
-        test_results.append(result['test_score'])  
-
-    return train_results, test_results
-
-
-def evaluation(estimator, trainX, trainy, testX, testy, label):
-
-    exp_decay = ExpDecay(init_temp=100, exp_const=0.2, min_temp=0.001)
-
-    #rhc Training
-    #rhc_nn = mlr.NeuralNetwork(hidden_nodes=[50, 30], algorithm=algorithm, learning_rate=0.001, max_attempts=200, max_iters=200, curve=True)
-    start_time = time.time()
-    estimator.fit(trainX, trainy)
-    training_time = time.time() - start_time
-    nn_loss = estimator.loss
-    #nn_fitness_curve = estimator.fitness_curve
-
-    #rhc Predict
-    start_time = time.time()
-    predy = estimator.predict(testX)
-    predict_time = time.time()- start_time
-
-    f1 = f1_score(testy,predy, average='weighted')
-    accuracy = accuracy_score(testy,predy)
-    precision = precision_score(testy,predy, average='weighted')
-    recall = recall_score(testy,predy, average='weighted')
-
-
-    sys.stdout = open(label +" test_score.txt", "w")
-
-    print("Model Evaluation Metrics Using Untouched Test Dataset")
-    print("*****************************************************")
-    print("Model Training Time (s):   "+"{:.5f}".format(training_time))
-    print("Model Prediction Time (s): "+"{:.5f}\n".format(predict_time))
-    print("F1 Score:  "+"{:.2f}".format(f1))
-    print("Accuracy:  "+"{:.2f}".format(accuracy)) 
-    print("Precision: "+"{:.2f}".format(precision)+"     Recall:    "+"{:.2f}".format(recall))
-    print("Loss: "+"{:.2f}".format(nn_loss))
-    print("*****************************************************")
-    sys.stdout.close()
-
-def run_varied_algorithms():
-    log('start to run neural network')
-    trainX, trainy, testX, testy = load_data()
-    exp_decay = ExpDecay(init_temp=100,
-                         exp_const=0.1,
-                         min_temp=0.001)
-
-    #rhl                     
-    nn_rhl = mlr.NeuralNetwork(hidden_nodes=[5, 3], activation='sigmiod',
-                           algorithm='random_hill_climb', max_iters=200,
-                           bias=True, is_classifier=True, learning_rate=0.01,
-                           early_stopping=False, clip_max=1e10,
-                           max_attempts=200, random_state=0, curve=False)
-
-    rhl_train_scores, rhl_test_scores = learning_curve(nn_rhl, trainX=trainX, trainy=trainy)
-    rhl_train_scores_mean = np.mean(rhl_train_scores, axis=1)
-    rhl_train_scores_std = np.std(rhl_train_scores, axis=1)
-    rhl_test_scores_mean = np.mean(rhl_test_scores, axis=1)
-    rhl_test_scores_std = np.std(rhl_test_scores, axis=1)
-
-    evaluation(nn_rhl, trainX=trainX, trainy=trainy, testX = testX, testy=testy, label = "rhl")
-
-    #sa
-    nn_sa = mlr.NeuralNetwork(hidden_nodes=[50, 30], activation='sigmiod',
-                           algorithm='simulated_annealing', schedule=exp_decay, max_iters=200,
-                           bias=True, is_classifier=True, learning_rate=0.001,
-                           early_stopping=False, clip_max=1e10,
-                           max_attempts=200, random_state=0, curve=False)
-
-    sa_train_scores, sa_test_scores = learning_curve(nn_sa, trainX=trainX, trainy=trainy)
-    sa_train_scores_mean = np.mean(sa_train_scores, axis=1)
-    sa_train_scores_std = np.std(sa_train_scores, axis=1)
-    sa_test_scores_mean = np.mean(sa_test_scores, axis=1)
-    sa_test_scores_std = np.std(sa_test_scores, axis=1)
-
-    evaluation(nn_sa, trainX=trainX, trainy=trainy, testX = testX, testy=testy, label = "sa")
-
-    #ga
-    nn_ga = mlr.NeuralNetwork(hidden_nodes=[50, 30], activation='sigmiod',
-                           algorithm='genetic_alg', schedule=exp_decay, max_iters=200,
-                           bias=True, is_classifier=True, learning_rate=0.001,
-                           early_stopping=False, clip_max=1e10,
-                           max_attempts=200, pop_size = 100, mutation_prob=0.2, random_state=0, curve=False)
-
-    ga_train_scores, ga_test_scores = learning_curve(nn_ga, trainX=trainX, trainy=trainy)
-    ga_train_scores_mean = np.mean(ga_train_scores, axis=1)
-    ga_train_scores_std = np.std(ga_train_scores, axis=1)
-    ga_test_scores_mean = np.mean(ga_test_scores, axis=1)
-    ga_test_scores_std = np.std(ga_test_scores, axis=1)
-
-    evaluation(nn_ga, trainX=trainX, trainy=trainy, testX = testX, testy=testy, label = "ga")   
-
-    #gd                    
-    nn_gd = mlr.NeuralNetwork(hidden_nodes=[50, 30], activation='sigmiod',
-                           algorithm='gradient_decent', max_iters=200,
-                           bias=True, is_classifier=True, learning_rate=0.001,
-                           early_stopping=False, clip_max=1e10,
-                           max_attempts=200, random_state=0, curve=False) 
-
-    gd_train_scores, gd_test_scores = learning_curve(nn_gd, trainX=trainX, trainy=trainy)
-    gd_train_scores_mean = np.mean(gd_train_scores, axis=1)
-    gd_train_scores_std = np.std(gd_train_scores, axis=1)
-    gd_test_scores_mean = np.mean(gd_test_scores, axis=1)
-    gd_test_scores_std = np.std(gd_test_scores, axis=1)
-
-    evaluation(nn_gd, trainX=trainX, trainy=trainy, testX = testX, testy=testy, label = "gd")   
-
-    #polt
-    train_sizes = (np.linspace(.25, 1.0, 5) * len(trainy)).astype('int')
     plt.figure()
-    plt.xlabel("iteration")
-    plt.ylabel("Score")
-    plt.title("training and validation score with different alorithm")
-    plt.grid()
+    plt.title(title + 'Mean accuracy vs training size')
+    plt.xlabel('Training size')
+    plt.ylabel('Mean Accuracy Score')
 
-    plt.fill_between(train_sizes, rhl_train_scores_mean - rhl_train_scores_std,
-                     rhl_train_scores_mean + rhl_train_scores_std, alpha=0.3)
-    plt.fill_between(train_sizes, rhl_test_scores_mean - rhl_test_scores_std,
-                     rhl_test_scores_mean + rhl_test_scores_std, alpha=0.3)
-    plt.plot(train_sizes, rhl_train_scores_mean, 'o-',
-             label="rhl training score")
-    plt.plot(train_sizes, rhl_test_scores_mean, 'o-',
-             label="rhl cross-validation score")
+    plt.plot(norm_train_szs, tr_means, 'o-', color='r', label='train')
+    plt.plot(norm_train_szs, tt_means, 'o-', color='g', label='test')
+    plt.fill_between(norm_train_szs, tr_means-tr_stdev, tr_means+tr_stdev, color='r', alpha=0.3)
+    plt.fill_between(norm_train_szs, tt_means-tt_stdev, tt_means+tt_stdev, color='g', alpha=0.3)
+    plt.legend(loc='best')
 
-    plt.fill_between(train_sizes, sa_train_scores_mean - sa_train_scores_std,
-                     sa_train_scores_mean + sa_train_scores_std, alpha=0.3)
-    plt.fill_between(train_sizes, sa_test_scores_mean - sa_test_scores_std,
-                     sa_test_scores_mean + sa_test_scores_std, alpha=0.3)
-    plt.plot(train_sizes, sa_train_scores_mean, 'o-',
-             label="sa training score")
-    plt.plot(train_sizes, sa_test_scores_mean, 'o-',
-             label="sa cross-validation score")
+    plt.savefig(full_name, format='png')
+    plt.close()
 
-    plt.fill_between(train_sizes, ga_train_scores_mean - ga_train_scores_std,
-                     ga_train_scores_mean + ga_train_scores_std, alpha=0.3)
-    plt.fill_between(train_sizes, ga_test_scores_mean - ga_test_scores_std,
-                     ga_test_scores_mean + ga_test_scores_std, alpha=0.3)
-    plt.plot(train_sizes, ga_train_scores_mean, 'o-',
-             label="ga training score")
-    plt.plot(train_sizes, ga_test_scores_mean, 'o-',
-             label="ga cross-validation score")
+def train(algorithm, X_train, X_test, y_train, y_test, X, y):
+    clf = mlrose_hiive.NeuralNetwork(hidden_nodes=[5, 2],activation='sigmoid',algorithm=algorithm, early_stopping=True,max_attempts=100, max_iters=5000,bias=True, learning_rate=.01,restarts=0, curve=True, random_state=2636)
+    start_time = time.time()
+    clf.fit(X_train, y_train)
+    runtime = time.time() - start_time
 
-    plt.fill_between(train_sizes, gd_train_scores_mean - gd_train_scores_std,
-                     gd_train_scores_mean + gd_train_scores_std, alpha=0.3)
-    plt.fill_between(train_sizes, gd_test_scores_mean - gd_test_scores_std,
-                     gd_test_scores_mean + gd_test_scores_std, alpha=0.3)
-    plt.plot(train_sizes, gd_train_scores_mean, 'o-',
-             label="gd training score")
-    plt.plot(train_sizes, gd_test_scores_mean, 'o-',
-             label="gd cross-validation score")  
+    y_pred = clf.predict(X_test)
+    final_score = accuracy_score(y_test, y_pred)
+    print(f"nn_{algorithm} accuracy: {final_score}")
+    print(f"{algorithm} runtime is {runtime} s")
 
-    plt.legend(loc="best")
-    plt.savefig('images/' +" score of neural network with differnt algorithms")
-       
-    
-if __name__ == "__main__":
+    fitness_curve = clf.fitness_curve
+    if algorithm != "gradient_descent":
+        fitness_curve = [i[0] for i in fitness_curve]
 
-    run_varied_algorithms()
+    plt.plot(fitness_curve)
+    plt.xlabel("Iterations")
+    plt.ylabel("Loss")
+    plt.title("Neural Network loss curve vs iterations for "+ algorithm)
+    plt.savefig(f"NN_{algorithm}_loss.png")
+    plt.clf()
+
+    # commented due to runtime, uncomment to generate validation curves
+    plot_learning_curve(clf, X, y, algorithm, f"{algorithm}_cv_curve.png")
+
+np.random.seed(2636)
+
+# Load Data
+print("Loading Breast Cancer Dataset")
+X, y = load_breast_cancer(return_X_y=True)
+scaler = preprocessing.StandardScaler().fit(X)
+X_scaled = scaler.transform(X)
+X_train, X_test, y_train, y_test = train_test_split(X_scaled, y)
+
+algorithms = ['gradient_descent', 'random_hill_climb', 'simulated_annealing', 'genetic_alg']
+
+for alg in algorithms:
+    train(alg, X_train, X_test, y_train, y_test, X_scaled, y)
--- assignment2/run.py
+++ assignment2/run.py
@@ -3,21 +3,20 @@ import os
 import flip_flop
 import four_peaks
 import travel_salesman
-import nn
 from helper import log
 
 if __name__ == "__main__":
     if not os.path.exists('images'):
         os.makedirs('images')
 
-    # log('start run_flip_flop')
-    # flip_flop.run_flip_flop()
+    log('start run_flip_flop')
+    flip_flop.run_flip_flop()
 
-    # log('start run_four_peaks')
-    # four_peaks.run_four_peaks()
+    log('start run_four_peaks')
+    four_peaks.run_four_peaks()
 
-    # log('start run_travel_salesman')
-    # travel_salesman.run_travel_salesman()
+    log('start run_travel_salesman')
+    travel_salesman.run_travel_salesman()
 
-    log('start nn')
-    nn.run_varied_algorithms()
+    # log('start nn')
+    # nn.run_varied_algorithms()
